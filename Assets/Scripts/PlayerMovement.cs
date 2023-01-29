c9bf104b253567cc8abd22d38e7ded2912ff2b0c
Steffen Møgelmose <steffen@mogelmose.org>
2023-01-29 17:39:10 +0100

    Updated project w. assets & scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToPoint();
+        }
+    }
+    
+    private void MoveToPoint() 
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
