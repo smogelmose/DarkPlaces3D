03c6e75e8850d4cf4979d24562b158a3e0f3cb6b
Steffen Møgelmose <steffen@mogelmose.org>
2023-11-24 17:19:56 +0100

    Added spawn system

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,42 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
-       [SerializeField] string sceneToLoad = "DarkScene2";
+       [SerializeField] int sceneToLoad = -1;
+       [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
-            {                
-                SceneManager.LoadScene(sceneToLoad);
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            Portal otherPortal = GetotherPortal();
+            UpdatePlayer(otherPortal);
+            Destroy(gameObject);
+        }
+
+        private static void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.transform.position;
+            player.transform.rotation = otherPortal.transform.rotation;
+        }
+
+        private Portal GetotherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+                return null;
             }
         }
     }
-}
